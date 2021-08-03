009547efe58672eb216865ca76716b8f92afe97e
kicaj29 <kicaj29@wp.pl>
2021-08-03 13:33:52 +0200

    csharp: creating from mongodb

--- AspNetCoreWebApiMongoDB/AspNetCoreWebApiMongoDB/Controllers/CrewController.cs
+++ AspNetCoreWebApiMongoDB/AspNetCoreWebApiMongoDB/Controllers/CrewController.cs
@@ -22,12 +22,21 @@ namespace AspNetCoreWebApiMongoDB.Controllers
         [HttpGet]
         public ActionResult<List<Crew>> Get() => this._crewService.GetCrews();
 
-        [HttpGet]
-        [Route("{id}")]
+        [HttpGet("/{id}", Name = "GetById")]
+        //[Route("{id}")]
         public ActionResult<Crew> Get(string id)
         {
             return this._crewService.GetCrew(id);
         }
-        
+
+        [HttpPost]
+        public ActionResult<Crew> Create(Crew c)
+        {
+            this._crewService.CreateCrew(c);
+
+            // https://stackoverflow.com/questions/25045604/can-anyone-explain-createdatroute-to-me/25110700
+            return CreatedAtRoute("GetById", new { id = c.Id }, c);
+        }
+
     }
 }

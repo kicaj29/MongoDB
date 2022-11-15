f6b665de3b88977cad69e6c21cf279ccef4a61a0
Jacek Kowalski <Jacek.Kowalski@hyland.com>
2022-11-15 10:22:49 +0100

    ApiFindOneAndUpdateAsync

---
+++ ApiFindOneAndUpdateAsync/ApiFindOneAndUpdateAsync/MongoPerson.cs
@@ -0,0 +1,18 @@
+﻿using MongoDB.Bson.Serialization.Attributes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ApiFindOneAndUpdateAsync
+{
+    public class MongoPerson
+    {
+        [BsonId]
+        public MongoDB.Bson.ObjectId Id { get; set; }
+
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+    }
+}
---
+++ ApiFindOneAndUpdateAsync/ApiFindOneAndUpdateAsync/Program.cs
@@ -0,0 +1,10 @@
+﻿// See https://aka.ms/new-console-template for more information
+using ApiFindOneAndUpdateAsync;
+
+Console.WriteLine("Hello, World!");
+
+Worker w = new Worker();
+await w.StartProcessing();
+
+Console.WriteLine("WORK FINISHED");
+Console.ReadKey();

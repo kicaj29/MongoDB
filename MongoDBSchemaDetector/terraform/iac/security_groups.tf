6c6f3d876e72891eb8076fb78dd4c50faead20d2
Jacek Kowalski <Jacek.Kowalski@hyland.com>
2023-01-10 21:42:10 +0100

    MongoDBSchemaDetector: security group update

--- MongoDBSchemaDetector/terraform/iac/security_groups.tf
+++ MongoDBSchemaDetector/terraform/iac/security_groups.tf
@@ -5,15 +5,12 @@ resource "aws_security_group" "lambda_security_group" {
   description = "Security group for lambda schema detector"
   vpc_id      = data.aws_vpc.lambda_vpc.id
 
-  ingress {
-    description      = "ingress"
-    from_port        = 0
-    to_port          = 65535
-    protocol         = "tcp"
-    cidr_blocks      = ["0.0.0.0/0"]
-    ipv6_cidr_blocks = ["::/0"]
-  }
+  // because this lambda is only triggered by S3 we prohibit any incomming traffic via ENI, this can be done because S3 does not use ENI
+  // no ingress means there are no rules and no rules means that all traffic via ENI is blocked 
+  // https://github.com/hashicorp/terraform-provider-aws/issues/4399
+  ingress = []
 
+  // egress has to set, without it the lambda is not triggered by the S3 (tested it)
   egress {
     from_port        = 0
     to_port          = 65535

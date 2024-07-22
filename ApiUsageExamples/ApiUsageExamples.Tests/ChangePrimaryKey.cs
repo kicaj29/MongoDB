a5c8b8a66b854a69ee0e46b9e712eb4305f1b253
Jacek Kowalski <Jacek.Kowalski@hyland.com>
2024-07-22 21:40:51 +0200

    ChangePK

---
+++ ApiUsageExamples/ApiUsageExamples.Tests/ChangePrimaryKey.cs
@@ -0,0 +1,40 @@
+﻿using MongoDB.Driver;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ApiUsageExamples.Tests
+{
+    internal class ChangePrimaryKey : BaseTest
+    {
+        [Test]
+        public async Task ChangePK()
+        {
+            // Arrange
+            var collection = DB.GetCollection<Person>("Persons");
+            Person person = new Person();
+            person.FirstName = "jacek";
+            person.LastName = "kowalski";
+            await collection.InsertOneAsync(person);
+            await collection.DeleteManyAsync(Builders<Person>.Filter.Empty);
+
+            // Act
+            var collectionNewPK = DB.GetCollection<PersonNewPK>("Persons");
+            PersonNewPK personNewPK = new PersonNewPK();
+            // Now FirstName will be PK
+            personNewPK.Id = "jacek";
+            personNewPK.LastName = "kowalski";
+            await collectionNewPK.InsertOneAsync(personNewPK);
+
+
+            // Assert
+            PersonNewPK actualDocument = await collectionNewPK.Find(Builders<PersonNewPK>.Filter.Eq(d => d.FirstName, personNewPK.FirstName)).Limit(1).FirstOrDefaultAsync();
+            Assert.IsNotNull(actualDocument);
+
+            // Cleanup
+            await collection.DeleteManyAsync(Builders<Person>.Filter.Empty);
+        }
+    }
+}
---
+++ ApiUsageExamples/ApiUsageExamples.Tests/MongoModels/PersonNewPK.cs
@@ -0,0 +1,19 @@
+﻿using MongoDB.Bson.Serialization.Attributes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ApiUsageExamples.Tests.MongoModels
+{
+    internal class PersonNewPK
+    {
+        public string Id { get; set; }
+
+        [BsonId]
+        [BsonRepresentation(BsonType.ObjectId)]
+        public string FirstName { get; set; } = default!;
+        public string LastName { get; set; } = default!;
+    }
+}
